611f8d1dd573b0f1b9c484c4dde14db8c3135c3e
Nandani-s <sudikshya589@gmail.com>
2025-12-11 18:07:45 +0545

    function script

--- script.js
+++ script.js
@@ -19,38 +19,38 @@
 // }
 
 
-const marks=[50,68,98,45]
-let totalmarks=0;
-let isfail = false
+// const marks=[50,68,98,45]
+// let totalmarks=0;
+// let isfail = false
 
-for(let i=0; i<marks.length; i++){
-	console.log(marks[i]);
-	if(marks[i]<35){
-		isfail=true;
-	}
-	totalmarks =marks[i]+totalmarks;
-}
-if(isfail){
-	console.log("you are fail");
-	return
-}
-
-
-
-
-// console.log(totalmarks);
-percentage=totalmarks/400*100;
+// for(let i=0; i<marks.length; i++){
+// 	console.log(marks[i]);
+// 	if(marks[i]<35){
+// 		isfail=true;
+// 	}
+// 	totalmarks =marks[i]+totalmarks;
+// }
+// if(isfail){
+// 	console.log("you are fail");
+// 	return
+// }
 
 
 
-let percentage1=Math.floor(percentage);{
-	console.log(percentage1,"%")
-}
-if(percentage>45){
-	console.log("you have passed");
-}else{
-	console.log("you have failed");
-}
- gpa=percentage/25;
- console.log("your GPA is",gpa);
+
+// // console.log(totalmarks);
+// percentage=totalmarks/400*100;
+
+
+
+// let percentage1=Math.floor(percentage);{
+// 	console.log(percentage1,"%")
+// }
+// if(percentage>45){
+// 	console.log("you have passed");
+// }else{
+// 	console.log("you have failed");
+// }
+//  gpa=percentage/25;
+//  console.log("your GPA is",gpa);
 
---
+++ select.js
@@ -0,0 +1,5 @@
+const name = prompt("your name");
+		const age = prompt("your age");
+		console.log(name);
+		console.log(age);
+		console.table([name, age])
